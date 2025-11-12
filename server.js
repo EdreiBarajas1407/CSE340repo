97887fd40894b017b1e10ba6b32428d50843d82d
Edrei Barajas <edrei.barajas14@gmail.com>
2025-11-11 20:17:50 -0600

    almost done 2.0

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 app.use("/inv", inventoryRoute)
-
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 /* ***********************
  * Index route
  *************************/
@@ -33,6 +36,23 @@ app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
 app.get("/", baseController.buildHome)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  console.dir(err);
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
