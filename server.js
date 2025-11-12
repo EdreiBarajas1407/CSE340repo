7c99d99e78adc825caab1cf4ec8d19d5bdf90b03
Edrei Barajas <edrei.barajas14@gmail.com>
2025-11-11 19:34:53 -0600

    hehehe

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-app.get("/", baseController.buildHome)
 
 /* ***********************
  * Index route
@@ -31,6 +30,7 @@ app.get("/", baseController.buildHome)
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
